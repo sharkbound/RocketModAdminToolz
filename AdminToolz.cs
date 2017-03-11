748eb14b1b8ca2606f8606c43e7c83582aa49c2f
sharkbound <laptopblaster@gmail.com>
2017-03-11 11:48:42 -0800

    added skeleton for the plugin code

---
+++ AdminToolz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.API;
+using Rocket.Unturned.Player;
+using Rocket.Core.Plugins;
+
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace AdminToolz
+{
+    public class AdminToolz : RocketPlugin<Config>
+    {
+        protected override void Load()
+        {
+            Logger.Log("AdminToolz has Loaded!");
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log("AdminToolz has Unloaded!");
+        }
+    }
+}
